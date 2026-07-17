afcf4b942dea7ef6ef22cddfb325b06816273ad9
blagodarnaya-ksysha <blagodarnaya.ksysha@mail.ru>
2026-07-17 13:52:43 +0300

    Blagodarnaya_Kseniya_4344: fixed CartOperationsTests.java

--- src/test/java/tests /CartOperationsTests.java
+++ src/test/java/tests /CartOperationsTests.java
@@ -9,6 +9,42 @@ import static utils.Constants.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CartOperationsTests extends BaseTest {
+  @Test @Order(1)
+    @DisplayName("3: Проверка перехода в корзину через меню")
+    public void Test03() {
+        MainPage.init().goToCart();
+        CartPage.init().waitForElement(TBODY_ID);
+        CartPage cartPage = CartPage.init();
+        assertTrue(cartPage.isCartTableVisible(), "Таблица корзины не отображается");
+        assertTrue(cartPage.areHeadersVisible(), "Колонки таблицы (Pic, Title, Price) не найдены");
+        assertTrue(cartPage.isPlaceOrderButtonVisible(), "Кнопка 'Place Order' отсутствует");
+    }
+
+    @Test @Order(2)
+    @DisplayName("4.1: Проверка сообщения после добавления товара")
+    public void Test04_1() {
+        MainPage.init().selectCategory(PH);
+        MainPage.init().openProduct(S6);
+        ProductPage.init().prepareAlertIntercept();
+        ProductPage.init().clickAddToCart();
+        com.codeborne.selenide.Selenide.sleep(1000);
+        String actualText = ProductPage.init().getCapturedAlertText();
+        assertTrue(actualText.contains(MSG_PRODUCT_ADDED),
+                "Ожидалось сообщение 'Product added', но получили: " + actualText);
+    }
+
+    @Test @Order(3)
+    @DisplayName("4.2: Отображение добавленного товара в корзине")
+    public void Test04_2() {
+        MainPage.init().selectCategory(PH);
+        MainPage.init().openProduct(S6);
+        ProductPage.init().clickAddToCart();
+        ProductPage.init().acceptAlert();
+        MainPage.init().goToCart();
+        com.codeborne.selenide.Selenide.sleep(2500);
+        assertTrue(CartPage.init().isProductPresent(S6),
+                "Товар 'Samsung galaxy s6' отсутствует в таблице корзины");
+    }
   
     @Test @Order(4)
     @DisplayName("5: Проверка добавления нескольких товаров в корзину")
